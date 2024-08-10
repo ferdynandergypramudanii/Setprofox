9d56412fcd8a994115201c811e1c30f2ea00bf28
ferdynand <ferdynandergypramudani11@gmail.com>
2024-08-10 13:25:02 +0700

    add ascii banner

--- setprox/disable_proxy.sh
+++ setprox/disable_proxy.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# Tentukan path ke profil Firefox (sesuaikan jika perlu)
+cat <<'EOF'
+ ____       _                    __           
+/ ___|  ___| |_ _ __  _ __ ___  / _| _____  __
+\___ \ / _ \ __| '_ \| '__/ _ \| |_ / _ \ \/ /
+ ___) |  __/ |_| |_) | | | (_) |  _| (_) >  < 
+|____/ \___|\__| .__/|_|  \___/|_|  \___/_/\_\
+               |_|                            
+EOF
+
 FIREFOX_PROFILE_PATH="$HOME/.mozilla/firefox/*.default-release"
 USER_JS_PATH="$FIREFOX_PROFILE_PATH/user.js"
 
-# Membuat atau menimpa file user.js untuk menonaktifkan proxy
 echo 'Menonaktifkan proxy di Firefox...'
 cat <<EOT > $USER_JS_PATH
 user_pref("network.proxy.type", 0);
--- setprox/enable_proxy.sh
+++ setprox/enable_proxy.sh
@@ -1,10 +1,18 @@
-#!/bin/bash     //shebang, yang menunjukkan bahwa skrip ini harus dijalankan menggunakan Bash shell. 
+#!/bin/bash  
 
-# Tentukan path ke profil Firefox (sesuaikan jika perlu)
+cat <<'EOF'
+ ____       _                    __           
+/ ___|  ___| |_ _ __  _ __ ___  / _| _____  __
+\___ \ / _ \ __| '_ \| '__/ _ \| |_ / _ \ \/ /
+ ___) |  __/ |_| |_) | | | (_) |  _| (_) >  < 
+|____/ \___|\__| .__/|_|  \___/|_|  \___/_/\_\
+               |_|                            
+EOF
+
+
 FIREFOX_PROFILE_PATH="$HOME/.mozilla/firefox/*.default-release"
 USER_JS_PATH="$FIREFOX_PROFILE_PATH/user.js"
 
-# Membuat atau menimpa file user.js untuk mengaktifkan proxy
 echo 'Mengaktifkan proxy Burp Suite di Firefox...'
 cat <<EOT > $USER_JS_PATH
 user_pref("network.proxy.type", 1);
